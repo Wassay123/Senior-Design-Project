96a8c1aa0a86e689a0c968e32cda663ec26890c6
Wassay123 <80175602+Wassay123@users.noreply.github.com>
2024-04-02 22:28:04 -0400

    Update model.py
    model now uses NYC coordinates for agents

--- models/virusModel/model.py
+++ models/virusModel/model.py
@@ -1,8 +1,10 @@
-import math
-import mesa
-import networkx as nx
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+from mesa.space import ContinuousSpace
+from mesa.datacollection import DataCollector
 from enum import Enum
-
+from flask import jsonify
+import math
 
 class State(Enum):
     SUSCEPTIBLE = 0
@@ -10,18 +12,14 @@ class State(Enum):
     RESISTANT = 2
     DEAD = 3
 
-
 def number_state(model, state):
-    return sum(1 for a in model.grid.get_all_cell_contents() if a.state is state)
-
+    return sum(1 for agent in model.schedule.agents if agent.state == state)
 
 def number_infected(model):
     return number_state(model, State.INFECTED)
 
-
 def number_susceptible(model):
     return number_state(model, State.SUSCEPTIBLE)
-
 
 def number_resistant(model):
     return number_state(model, State.RESISTANT)
@@ -29,72 +27,64 @@ def number_resistant(model):
 def number_dead(model):
     return number_state(model, State.DEAD)
 
+def scale_coordinates(coord, min_lat, max_lat, min_long, max_long, scale_x, scale_y, margin=5):
+    scaled_x = margin + ((coord[1] - min_long) / (max_long - min_long)) * (scale_x - 2 * margin)
+    scaled_y = margin + ((coord[0] - min_lat) / (max_lat - min_lat)) * (scale_y - 2 * margin)
+    return (scaled_x, scaled_y)
 
 
-class VirusOnNetwork(mesa.Model):
-    """A virus model with some number of agents"""
 
-    def __init__(
-        self,
-        num_nodes=10,
-        avg_node_degree=3,
-        initial_outbreak_size=1,
-        virus_spread_radius=1,
-        virus_spread_chance=0.4,
-        virus_check_frequency=0.4,
-        recovery_chance=0.3,
-        gain_resistance_chance=0.5,
-        death_rate=0.2
-        
-    ):
-        self.num_nodes = num_nodes
-        prob = avg_node_degree / self.num_nodes
-        self.G = nx.erdos_renyi_graph(n=self.num_nodes, p=prob)
-        self.grid = mesa.space.NetworkGrid(self.G)
-        self.schedule = mesa.time.RandomActivation(self)
-        self.initial_outbreak_size = (
-            initial_outbreak_size if initial_outbreak_size <= num_nodes else num_nodes
-        )
+class VirusOnNetwork(Model):
+    def __init__(self, agents_data, width=100, height=100, virus_spread_radius=20, virus_spread_chance=0.4, virus_check_frequency=0.4, recovery_chance=0.3, gain_resistance_chance=0.5, death_rate=0.2):
+        self.schedule = RandomActivation(self)
+        self.space = ContinuousSpace(width, height, torus=False)
+        self.virus_spread_radius = virus_spread_radius
         self.virus_spread_chance = virus_spread_chance
         self.virus_check_frequency = virus_check_frequency
         self.recovery_chance = recovery_chance
         self.gain_resistance_chance = gain_resistance_chance
-        self.virus_spread_radius = virus_spread_radius
         self.death_rate = death_rate
 
-        self.datacollector = mesa.DataCollector(
-            {
-                "Infected": number_infected,
-                "Susceptible": number_susceptible,
-                "Resistant": number_resistant,
-                "Death": number_dead
-            }
-        )
+        # Assuming min and max values for latitude and longitude are known or can be calculated from agents_data
+        min_lat = min(agent['coordinates'][0] for agent in agents_data)
+        max_lat = max(agent['coordinates'][0] for agent in agents_data)
+        min_long = min(agent['coordinates'][1] for agent in agents_data)
+        max_long = max(agent['coordinates'][1] for agent in agents_data)
 
-        # Create agents
-        for i, node in enumerate(self.G.nodes()):
-            a = VirusAgent(
-                i,
-                self,
-                State.SUSCEPTIBLE,
-                self.virus_spread_radius,
-                self.virus_spread_chance,
-                self.virus_check_frequency,
-                self.recovery_chance,
-                self.gain_resistance_chance,
-                self.death_rate
-            )
-            self.schedule.add(a)
-            # Add the agent to the node
-            self.grid.place_agent(a, node)
+        for i, agent_info in enumerate(agents_data):
+            # Use the provided scale_coordinates function
+            pos = scale_coordinates(agent_info['coordinates'], min_lat, max_lat, min_long, max_long, width, height)
+            
+            # Determine the agent's state based on the color
+            if agent_info['color'] == 'green':
+                state = State.SUSCEPTIBLE
+            elif agent_info['color'] == 'red':
+                state = State.INFECTED
+            else:
+                # Default or unspecified color leads to SUSCEPTIBLE state; adjust as needed
+                state = State.SUSCEPTIBLE
+            
+            agent = VirusAgent(i, self, pos, state, self.virus_spread_radius, self.virus_spread_chance, self.virus_check_frequency, self.recovery_chance, self.gain_resistance_chance, self.death_rate)
+            self.schedule.add(agent)
+            self.space.place_agent(agent, pos)
 
-        # Infect some nodes
-        infected_nodes = self.random.sample(list(self.G), self.initial_outbreak_size)
-        for a in self.grid.get_cell_list_contents(infected_nodes):
-            a.state = State.INFECTED
+        self.datacollector = DataCollector({
+            "Susceptible": lambda m: m.number_state(State.SUSCEPTIBLE),
+            "Infected": lambda m: m.number_state(State.INFECTED),
+            "Resistant": lambda m: m.number_state(State.RESISTANT),
+            "Dead": lambda m: m.number_state(State.DEAD)
+        })
 
-        self.running = True
+    def number_state(self, state):
+        """Count agents by state."""
+        return sum(1 for agent in self.schedule.agents if agent.state is state)
+
+    def step(self):
         self.datacollector.collect(self)
+        self.schedule.step()
+
+    def get_agents_states(self):
+        return [agent.state.value for agent in self.schedule.agents]
 
     def resistant_susceptible_ratio(self):
         try:
@@ -104,56 +94,29 @@ class VirusOnNetwork(mesa.Model):
         except ZeroDivisionError:
             return math.inf
 
-    def step(self):
-        self.schedule.step()
-        # collect data
-        self.datacollector.collect(self)
 
-    def run_model(self, n):
-        for i in range(n):
-            self.step()
-
-
-class VirusAgent(mesa.Agent):
-    def __init__(
-        self,
-        unique_id,
-        model,
-        initial_state,
-        virus_spread_radius,
-        virus_spread_chance,
-        virus_check_frequency,
-        recovery_chance,
-        gain_resistance_chance,
-        death_rate
-    ):
+class VirusAgent(Agent):
+    def __init__(self, unique_id, model, pos, state, virus_spread_radius, virus_spread_chance, virus_check_frequency, recovery_chance, gain_resistance_chance, death_rate):
         super().__init__(unique_id, model)
-
-        self.state = initial_state
-
+        self.pos = pos  # Store the position of the agent
+        self.state = state
+        self.virus_spread_radius = virus_spread_radius
         self.virus_spread_chance = virus_spread_chance
         self.virus_check_frequency = virus_check_frequency
         self.recovery_chance = recovery_chance
         self.gain_resistance_chance = gain_resistance_chance
-        self.virus_spread_radius = virus_spread_radius
         self.death_rate = death_rate
 
     def try_to_infect_neighbors(self):
-        #check all agents in radius r from self
-        #if agents are in that radius then try to infect them
-        #else do nothing
-
-        neighbors_nodes = self.model.grid.get_neighborhood(
-            self.pos, include_center=False, radius=self.virus_spread_radius
-        )
-        susceptible_neighbors = [
-            agent
-            for agent in self.model.grid.get_cell_list_contents(neighbors_nodes)
-            if agent.state is State.SUSCEPTIBLE
-        ]
-        for a in susceptible_neighbors:
+        # Find neighbors within a specified radius in ContinuousSpace
+        neighbors = self.model.space.get_neighbors(self.pos, self.virus_spread_radius, include_center=False)
+        susceptible_neighbors = [agent for agent in neighbors if agent.state == State.SUSCEPTIBLE]
+        
+        # Attempt to infect susceptible neighbors
+        for neighbor in susceptible_neighbors:
             if self.random.random() < self.virus_spread_chance:
-                a.state = State.INFECTED
+                neighbor.state = State.INFECTED
+
 
     def try_gain_resistance(self):
         if self.random.random() < self.gain_resistance_chance:
@@ -185,34 +148,19 @@ class VirusAgent(mesa.Agent):
 
 
 if __name__ == "__main__":
-    num_nodes = 50
-    avg_node_degree = 5
-    initial_outbreak_size = 1
-    virus_spread_radius = 1
-    virus_spread_chance = 0.4
-    virus_check_frequency = 0.4
-    recovery_chance = 0.3
-    gain_resistance_chance = 0.5
-    death_rate = 0.1  # Set your desired death rate
+    agents = [{'coordinates': [40.6459870823861, -73.97978442274959], 'color': 'green'}, {'coordinates': [40.86358032074523, -73.89109945613701], 'color': 'green'}, {'coordinates': [40.61729281571782, -73.94045017276409], 'color': 'green'}, {'coordinates': [40.67673076357593, -73.78729054175422], 'color': 'green'}, {'coordinates': [40.726698804986825, -73.81926901302002], 'color': 'green'}, {'coordinates': [40.60008237240675, -74.17196392064943], 'color': 'green'}, {'coordinates': [40.83211308171965, -73.92069553875987], 'color': 'green'}, {'coordinates': [40.84777267302635, -73.89056725085115], 'color': 'green'}, {'coordinates': [40.55598140484815, -74.18904757331794], 'color': 'green'}, {'coordinates': [40.89091139834255, -73.8185322254578], 'color': 'red'}]
 
     # Create an instance of the model
-    model = VirusOnNetwork(
-        num_nodes=num_nodes,
-        avg_node_degree=avg_node_degree,
-        initial_outbreak_size=initial_outbreak_size,
-        virus_spread_radius=virus_spread_radius,
-        virus_spread_chance=virus_spread_chance,
-        virus_check_frequency=virus_check_frequency,
-        recovery_chance=recovery_chance,
-        gain_resistance_chance=gain_resistance_chance,
-        death_rate=death_rate
-    )
+    model = VirusOnNetwork(agents)
 
     # Run the model for a certain number of steps
-    num_steps = 50
-    model.run_model(num_steps)
+    for i in range(10):  # Run the model for 10 steps, or any number you choose
+        print("Susceptible : {}".format(number_susceptible(model)))
+        print("Infected : {}".format(number_infected(model)))
+        print("Resistant : {}".format(number_resistant(model)))
+        print("Dead : {}".format(number_dead(model)))
+        print(model.get_agents_states())
+        print()
 
-    print("Susceptible : {}".format(number_susceptible(model)))
-    print("Infected : {}".format(number_infected(model)))
-    print("Resistant : {}".format(number_resistant(model)))
-    print("Dead : {}".format(number_dead(model)))
+        model.step()
+
