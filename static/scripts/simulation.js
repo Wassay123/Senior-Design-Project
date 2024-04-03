c5db7b5eab6413dbba1734858f71cebc70a6f056
Wassay123 <80175602+Wassay123@users.noreply.github.com>
2024-04-02 22:31:46 -0400

    Update simulation.js

--- static/scripts/simulation.js
+++ static/scripts/simulation.js
@@ -1,8 +1,9 @@
 // Define web socket at local port (Must be changed if running on different URL)
-var socket = io.connect('http://127.0.0.1:5000');
+var socket = io.connect("http://127.0.0.1:5000");
 
 // Define global variables
 var simulationState = "idle";  // Possible simulationStates: "idle", "sending"
+var mapState = "idle"; // Possible simulationStates: "idle", "populated"
 var simulationInterval;
 var modelType; // values can be "virus" or "tornado"
 
@@ -10,23 +11,40 @@ var modelType; // values can be "virus" or "tornado"
 document.addEventListener("DOMContentLoaded", function() {
     var virusModelBtn = document.getElementById("virusModelBtn");
     selectModel(virusModelBtn);
-    switchDensityImage();
 });
 
+
 // Update the HTML elements with the simulation model data
-socket.on('update_event', function (data) {
+socket.on("update_event", function (data) {
     if (modelType == "virus"){
         updateSimulationInfo(data);
         updateVirusSimulationCharts(data);
-        updateVisualization();
-
-    } else if (modelType == "tornado"){
-
+        updateVirusVisuals(data);
+    } 
+    else if (modelType == "tornado"){
         updateSimulationInfo(data);
         updateTornadoSimulationCharts(data);
-        //add more here for visualization through other function
     } 
+    
 });
+
+function placeVirusAgents(){
+    const numAgents = parseFloat(document.getElementById("slider2").value);
+    const numInfected = parseFloat(document.getElementById("slider3").value);
+    const numSusceptible = numAgents - numInfected;
+
+    placeAgents(numSusceptible, "green");
+    placeAgents(numInfected, "red");
+
+    mapState = "populated";
+}
+
+function placeTornadoAgents(){
+    const numAgents = parseFloat(document.getElementById("slider2").value);
+    placeAgents(numAgents, "green");
+
+    mapState = "populated";
+}
 
 function startSimulation() {
     // If the simulation is already running, then do nothing
@@ -36,14 +54,13 @@ function startSimulation() {
 
     var current_step = 1;
 
+    // Disable start button to prevent users spamming the button
     var startSimulationButton = document.getElementById("startSimulationButton");
     startSimulationButton.disabled = true;
     startSimulationButton.style.color = "grey";
 
-    const sliders = document.querySelectorAll('.allTheSliders');
-    
-    // append model type to values list
     let values = []; 
+    const sliders = document.querySelectorAll(".simulationSliders");
 
     // Loop through all sliders and get their values
     sliders.forEach(slider => {
@@ -51,12 +68,24 @@ function startSimulation() {
         values.push(sliderValue);
     });
 
-    const simulationSpeed = parseFloat(sliders[sliders.length - 1].value);
-    const densityRange = document.getElementById("densityRangeDropdown").value;  // Get selected density range
+    if (modelType == "virus" && mapState == "idle"){
+        placeVirusAgents();
+    }
 
-    socket.emit('start_simulation', {model_type: modelType, values: values, density_range: densityRange});
+    if (modelType == "tornado" && mapState == "idle"){
+        placeTornadoAgents();
+    }
 
-    // Define an interval function which will be repeated every Math.floor(1000 / simulationSpeed)) milliseconds
+    socket.emit('simulation_points', {agent_points: markerData});
+
+    mapState = "populated";
+
+    const maxSteps = parseFloat(document.getElementById("slider1").value);
+    const simulationSpeed = parseFloat(document.getElementById("slider8").value);
+
+    socket.emit("start_simulation", {slider_values: values});
+
+    // Define an interval function which will be repeated every Math.floor(5000 / simulationSpeed)) milliseconds
     simulationInterval = setInterval(function () {
         // If the State is idle then the Stop button was pressed and the simulation should end
         if (simulationState == "idle") {
@@ -64,15 +93,15 @@ function startSimulation() {
             return;
         }
 
-        socket.emit('simulation_update_request', {step: current_step});
+        socket.emit("simulation_step", {step: current_step});
         current_step++;
 
         // Stop the simulation when all steps are simulated (values[1] holds the total numSteps for the model to take)
-        if (current_step > values[1]) {
+        if (current_step > maxSteps) {
             stopSimulation();
         }
 
-    }, Math.floor(5000 / simulationSpeed));
+    }, Math.floor(2500 / simulationSpeed));
 
     simulationState = "sending";
 }
@@ -84,19 +113,26 @@ function resetSimulation() {
     if (simulationState == "sending"){
         return;
     }
-
-    // Reset NYC map to be blank
-    var imgElement = document.getElementById("simulationMap");
-    imgElement.src = 'static/images/nyc_basemap_empty.png?' + new Date().getTime();
-
     let defaultValues;
 
-    if (modelType == "virus") {
-        // default slider values
-        const defaultValues = [10, 10, 1, 1, 0.4, 0.3, 0.2, 3];
+    resetMap();
+    resetMarkers();
 
+    if (modelType == "virus"){
+        resetVirusSliders();
+        placeVirusAgents();
+    }
+
+    if (modelType == "tornado"){
+        resetTornadoSliders();
+        placeTornadoAgents();
+        defineTornadoAndCities();
+    }
+
+
+    if (modelType == "virus") {
         // reset meta data about the simulation
-        document.getElementById("simulationStep").innerHTML = "Days: ";
+        document.getElementById("simulationStep").innerHTML = "Steps: ";
         document.getElementById("statistic1").innerHTML = "Infected: ";
         document.getElementById("statistic2").innerHTML = "Susceptible: ";
         document.getElementById("statistic3").innerHTML = "Resistant: ";
@@ -105,10 +141,10 @@ function resetSimulation() {
         // code to initialize the charts
         const numInfected = document.getElementById(`slider3`).value;
 
-        statistic1 = numInfected
-        statistic2 = 10 - statistic1
-        statistic3 = 0
-        deadCount = 0
+        statistic1 = numInfected;
+        statistic2 = 10 - statistic1;
+        statistic3 = 0;
+        deadCount = 0;
     
         pieChart.data.datasets[0].data = [statistic1, statistic2, statistic3, deadCount];
         pieChart.update();
@@ -121,45 +157,35 @@ function resetSimulation() {
     }
 
     if (modelType == "tornado") {
-        // default slider values
-        const defaultValues = [10, 10, 1, 0.5, 1, 0.5, 0.1, 3];
-
         // reset meta data about the simulation
-        document.getElementById("simulationStep").innerHTML = "Days: ";
+        document.getElementById("simulationStep").innerHTML = "Steps: ";
         document.getElementById("statistic1").innerHTML = "Safe: ";
         document.getElementById("statistic2").innerHTML = "Injured: ";
         document.getElementById("statistic3").innerHTML = "Dead: ";
-        document.getElementById("statistic4").innerHTML = "Days until first casuality: ";
 
-        // Add chart functionality here
+        // code to initialize the charts
+        const numAgents = document.getElementById(`slider2`).value;
+
+        statistic1 = 0;
+        statistic2 = numAgents;
+        statistic3 = 0;
+        deadCount = 0;
+    
+        pieChart.data.datasets[0].data = [statistic1, statistic2, statistic3, deadCount];
+        pieChart.update();
+    
+        lineChart.data.datasets[0].data = [statistic1, statistic2, statistic3, deadCount];
+        lineChart.update();
+    
+        barChart.data.datasets[0].data = [statistic1, statistic2, statistic3, deadCount];
+        barChart.update();
     }
 
-    const sliders = document.querySelectorAll('.allTheSliders');
-
-    sliders.forEach((slider, i) => {
-        slider.value = defaultValues[i];
-    });
-}
-
-
-// Function to stop the simulation
-function stopSimulation() {
-    // If the simulation is not running return
-    if (simulationState == "idle") {
-        return;
-    }
-
-    // Else Reset the Interval function and set the simulation to idle
-    var startSimulationButton = document.getElementById("startSimulationButton");
-    startSimulationButton.disabled = false;
-    startSimulationButton.style.color = "white";
-
-    clearInterval(simulationInterval);
-    simulationState = "idle";
 }
 
 function updateValue(sliderName, sliderValue) {
     document.getElementById(sliderName).innerText = sliderValue;
+    mapState = "idle";
 }
 
 function makeParameterTitleBold(message) {
@@ -179,32 +205,77 @@ function revertParameterTitle(message) {
     parameterName.style.textShadow = '';
 }
 
+
+function resetTornadoSliders(){
+    // default slider values
+    const defaultValues = [10, 10, 1, 0.5, 1, 0.5, 0.1, 3];
+
+    const sliders = document.querySelectorAll(".allTheSliders");
+
+    sliders.forEach((slider, i) => {
+        slider.value = defaultValues[i];
+
+        const valueDisplayId = `slider${i + 1}Value`;
+        const valueDisplay = document.getElementById(valueDisplayId);
+        if (valueDisplay) {
+            valueDisplay.innerHTML = defaultValues[i].toString();
+        }
+    });
+}
+
+function resetVirusSliders(){
+    // default slider values
+    const defaultValues = [10, 10, 1, 1, 0.4, 0.3, 0.2, 3];
+
+    const sliders = document.querySelectorAll(".allTheSliders");
+
+    sliders.forEach((slider, i) => {
+        slider.value = defaultValues[i];
+
+        const valueDisplayId = `slider${i + 1}Value`;
+        const valueDisplay = document.getElementById(valueDisplayId);
+        if (valueDisplay) {
+            valueDisplay.innerHTML = defaultValues[i].toString();
+        }
+    });
+}
+// Function to stop the simulation
+function stopSimulation() {
+    // If the simulation is not running return
+    if (simulationState == "idle") {
+        return;
+    }
+
+    // Else Reset the Interval function and set the simulation to idle
+    var startSimulationButton = document.getElementById("startSimulationButton");
+    startSimulationButton.disabled = false;
+    startSimulationButton.style.color = "white";
+
+    clearInterval(simulationInterval);
+    simulationState = "idle";
+}
+
 // Function to update the HTML elements with the simulation information
 function updateSimulationInfo(result) {
-    document.getElementById("simulationStep").innerHTML = "Days: " + result.step;
+    document.getElementById("simulationStep").innerHTML = "Step: " + result.step;
     
     if (modelType == "tornado") {
+        document.getElementById("statistic4").style.display = "none";
+
         document.getElementById("statistic1").innerHTML = "Safe: " + result.safe;
         document.getElementById("statistic2").innerHTML = "Injured: " + result.injured;
         document.getElementById("statistic3").innerHTML = "Dead: " + result.dead;
-        document.getElementById("statistic4").innerHTML = "Days until first casuality:" + result.days_til_cas;
 
-    } else if (modelType == "virus") {
+        //document.getElementById("statistic4").innerHTML = "Days until first casuality:" + result.days_til_cas;
+    } 
+    else if (modelType == "virus") {
+        document.getElementById("statistic4").style.display = "flex";
+
         document.getElementById("statistic1").innerHTML = "Infected: " + result.infected;
         document.getElementById("statistic2").innerHTML = "Susceptible: " + result.susceptible;
         document.getElementById("statistic3").innerHTML = "Resistant: " + result.resistant;
-        document.getElementById("statistic4").style.display = "flex";
         document.getElementById("statistic4").innerHTML = "Dead: " + result.dead;
     }
-}
-
-
-// Function to update the visualization
-function updateVisualization() {
-    var imgElement = document.getElementById("simulationMap");
-
-    // Update the image source directly
-    imgElement.src = 'static/images/nyc_basemap.png?' + new Date().getTime();
 }
 
 function updateVirusSimulationCharts(result){
@@ -238,24 +309,28 @@ function updateTornadoSimulationCharts(result){
     barChart.update();
 }
 
+function updateVirusVisuals(data){
+    var agent_states = data.agent_states;
+    updateMarkers(agent_states);
+}
+
 function selectModel(button) {
-    var buttons = document.querySelectorAll('.simulationButtons');
+    var buttons = document.querySelectorAll(".simulationButtons");
 
-    buttons.forEach(function(btn) {
-        btn.classList.remove('selected');
-    });
-
-    button.classList.add('selected');
+    buttons.forEach(function(btn) {btn.classList.remove("selected");});
+    button.classList.add("selected");
 
     if (button.id === "virusModelBtn") {
         selectVirusModel();
         modelType = "virus";
     } 
-    
-    if (button.id === "tornadoModelBtn") {
+    else if (button.id === "tornadoModelBtn") {
         selectTornadoModel();
         modelType = "tornado";
     }
+
+    mapState = "populated";
+    socket.emit("model_type_change", {model_type: modelType})
 }
 
 function updateSliderText(sliderName, Title) {
@@ -299,20 +374,26 @@ function selectTornadoModel() {
     updateSliderText("slider8Name", "Simulation Speed");
     updateSliderRange("slider8", 1, 3, 3, 1);
 
+    //update simulation informational stats to focus on tornado model
+    document.getElementById("statistic4").style.display = "none";
 
-    document.getElementById("statistic1").innerText = "Safe";
-    document.getElementById("statistic2").innerText = "Injured";
-    document.getElementById("statistic3").innerText = "Dead";
-    document.getElementById("statistic4").innerText = "Days until first casuality:";
+    document.getElementById("simulationStep").innerText = "Steps:";
+    document.getElementById("statistic1").innerText = "Safe:";
+    document.getElementById("statistic2").innerText = "Injured:";
+    document.getElementById("statistic3").innerText = "Dead:";
+
+    //update color key to focus on tornado model
+    document.getElementById("deadRectangle").style.display = "none";
+    document.getElementById("deadLabel").style.display = "none";
 
     document.getElementById("susceptibleLabel").innerText = "Safe";
     document.getElementById("resistantLabel").innerText = "Injured";
-    document.getElementById("deadLabel").innerText = "Dead";
-    document.getElementById("infectedRectangle").style.display = "none";
-    document.getElementById("infectedLabel").style.display = "none";
+    document.getElementById("infectedLabel").innerText = "Dead";
 
-    document.getElementById('visualsContainer').style.display = "flex";
-    animateBall();
+    resetTornadoSliders();
+    resetVirusSliders();
+    initializeTornadoMap();
+    mapState = "populated";
 }
 
 function selectVirusModel() {
@@ -341,96 +422,28 @@ function selectVirusModel() {
     updateSliderText("slider8Name", "Simulation Speed");
     updateSliderRange("slider8", 1, 3, 3, 1);
 
-
-    document.getElementById("statistic1").innerText = "Infected";
-    document.getElementById("statistic2").innerText = "Susceptible";
-    document.getElementById("statistic3").innerText = "Resistant";
+    //update simulation informational stats to focus on virus model
     document.getElementById("statistic4").style.display = "flex";
-    document.getElementById("statistic4").innerText = "Dead";
+
+    document.getElementById("simulationStep").innerText = "Steps:";
+    document.getElementById("statistic1").innerText = "Infected:";
+    document.getElementById("statistic2").innerText = "Susceptible:";
+    document.getElementById("statistic3").innerText = "Resistant:";
+    document.getElementById("statistic4").innerText = "Dead:";
+
+    //update color key to focus on virus model
+    document.getElementById("deadRectangle").style.display = "flex";
+    document.getElementById("deadLabel").style.display = "flex";
 
     document.getElementById("susceptibleLabel").innerText = "Susceptible";
     document.getElementById("resistantLabel").innerText = "Resistant";
-    document.getElementById("infectedRectangle").style.display = "flex";
-    document.getElementById("infectedLabel").style.display = "flex";
     document.getElementById("infectedLabel").innerText = "Infected";
-    document.getElementById("deadLabel").innerText = "Dead";
+    document.getElementById("deadLabel").innerHTML = "Dead";
 
-    document.getElementById('visualsContainer').style.display = "none";
-}
-//Funtion to change image based on density dropdown choice
-function switchDensityImage(){
-    var dropdown = document.getElementById("densityRangeDropdown");
-    var selectedValue = dropdown.value;
-    var imgElement = document.getElementById("simulationMap");
-    var imagePath = '';
-
-    switch (selectedValue) {
-        case "0,10000":
-            imagePath = "../static/images/nyc_basemap_density_0_10000.png";
-            break;
-        case "10001,25000":
-            imagePath = "../static/images/nyc_basemap_density_10001_25000.png";
-            break;
-        case "25001,50000":
-            imagePath = "../static/images/nyc_basemap_density_25001_50000.png";
-            break;
-        case "50001,inf":
-            imagePath = "../static/images/nyc_basemap_density_50001_inf.png";
-            break;
-
-    }
-    imgElement.src = imagePath;
+    resetTornadoSliders();
+    resetVirusSliders();
+    initializeVirusMap();
+    mapState = "populated";
 }
 
 
-
-var animationInterval;
-var pathPoints = []; // Array to store path points
-
-    function animateBall() {
-        // Get the ball element
-        var ball = document.getElementById("ball");
-        var pathLine = document.getElementById("pathLine");
-
-        // Reset ball to initial position
-        ball.style.left = "0px";
-        ball.style.top = (window.innerHeight / 2) + "px";
-        pathPoints = []; // Clear path points
-
-        // Reset path line
-        pathLine.setAttribute("d", "");
-
-        // Animate the ball
-        var currentPositionX = 0;
-        var currentPositionY = parseFloat(ball.style.top);
-        var directionY = 1; // 1 for moving downwards, -1 for moving upwards
-        animationInterval = setInterval(function() {
-            if (currentPositionX < 720) {
-                currentPositionX += 3; // Adjust speed by changing increment value
-                currentPositionY += directionY * (Math.floor(Math.random() * 21) - 10); // Random change in Y position between -10 and 10
-                ball.style.left = currentPositionX + "px";
-                ball.style.top = currentPositionY + "px";
-                pathPoints.push({ x: currentPositionX, y: currentPositionY }); // Store current position in pathPoints array
-                updatePath(pathLine, pathPoints); // Update path line
-            } else {
-                clearInterval(animationInterval);
-            }
-
-            // Check if ball reaches top or bottom of the screen
-            if (currentPositionY <= 0) {
-                directionY = 1; // Change direction to move downwards
-            } else if (currentPositionY >= window.innerHeight - ball.offsetHeight) {
-                directionY = -1; // Change direction to move upwards
-            }
-        }, 35); // Adjust speed by changing interval value (higher value means slower animation)
-    }
-
-    function updatePath(pathElement, points) {
-        // Generate SVG path string from points
-        var pathString = "M " + points[0].x + " " + points[0].y;
-        for (var i = 1; i < points.length; i++) {
-            pathString += " L " + points[i].x + " " + points[i].y;
-        }
-        pathElement.setAttribute("d", pathString);
-        
-    }
